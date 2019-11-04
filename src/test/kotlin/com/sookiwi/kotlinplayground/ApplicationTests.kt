6ff57f36d8bac5927e05530620aba5ad173a6f89
Kim Soo <k.soo@linecorp.com>
2019-11-05 00:19:36 +0900

    init

---
+++ src/test/kotlin/com/sookiwi/kotlinplayground/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sookiwi.kotlinplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
